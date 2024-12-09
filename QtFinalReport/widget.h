77cecbefddb2e2e9b0bac8f842fc2ff25520340b
41243144 <41243144@nfu.edu.tw>
2024-12-09 15:10:18 +0800

    Add project

---
+++ QtFinalReport/widget.h
@@ -0,0 +1,22 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QMqttClient>
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+private slots:
+    void onMessageReceived(const QByteArray &message, const QMqttTopicName &topic); // 處理接收到的訊息
+    void subscribeToTopic();
+
+private:
+    QMqttClient *mqttClient;         // MQTT 客戶端
+
+};
+#endif // WIDGET_H
